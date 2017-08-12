5592aacf94ca37d6cfcdc7c65c2c20abf19ed726
Gary O Brien <gary.obrien@students.ittralee.ie>
2017-08-13 00:54:29 +0100

    Added accountName to Account.java

--- BankSystem/src/Account.java
+++ BankSystem/src/Account.java
@@ -2,13 +2,17 @@
  * Created by Gary on 12/08/2017.
  */
 public abstract class Account {
+    private String accountName;
     private int accountId;
     private float balance = 0.0f;
     private boolean isActive;
 
 
-    public Account(int accountId, boolean isActive) {
+    public Account(String accountName, int accountId, boolean isActive) {
+        this.accountName = accountName;
         this.accountId = accountId;
         this.isActive = isActive;
     }
+
+
 }
