2532c5b4b14b610823098d07a8a2d4654248cbf0
Gary O Brien <gary.obrien@students.ittralee.ie>
2017-08-13 19:00:12 +0100

    Added getters and setters for interestRate attribute

--- BankSystem/src/SavingsAccount.java
+++ BankSystem/src/SavingsAccount.java
@@ -2,11 +2,20 @@
  * Created by Gary on 13/08/2017.
  */
 public class SavingsAccount extends Account{
-    private float interestRate;
 
+    private float interestRate;
 
     public SavingsAccount(String accountName, int accountId, boolean isActive, float interestRate) {
         super(accountName, accountId, isActive);
         this.interestRate = interestRate;
     }
+
+    public float getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(float interestRate) {
+        this.interestRate = interestRate;
+    }
+    
 }
