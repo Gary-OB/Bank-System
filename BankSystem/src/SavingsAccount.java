7dc89e9a318de3d60e53b3fb57c215caa4a50ffb
Gary O Brien <gary.obrien@students.ittralee.ie>
2017-08-13 18:53:39 +0100

    Added constructor for SavingsAccount.java

--- BankSystem/src/SavingsAccount.java
+++ BankSystem/src/SavingsAccount.java
@@ -1,5 +1,12 @@
 /**
  * Created by Gary on 13/08/2017.
  */
-public class SavingsAccount {
+public class SavingsAccount extends Account{
+    private float interestRate;
+
+
+    public SavingsAccount(String accountName, int accountId, boolean isActive, float interestRate) {
+        super(accountName, accountId, isActive);
+        this.interestRate = interestRate;
+    }
 }
