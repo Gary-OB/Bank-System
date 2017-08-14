9bd6ca6ecaab5a0889648c274b258f60437a5054
Gary O Brien <gary.obrien@students.ittralee.ie>
2017-08-14 21:43:51 +0100

    Added currentAccount ArrayList to AccountHolder.java

--- BankSystem/src/AccountHolder.java
+++ BankSystem/src/AccountHolder.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Gary on 12/08/2017.
  */
@@ -6,4 +9,5 @@ public class AccountHolder {
     private String accountHolderId;
     private String surname;
     private String forename;
+    private List<CurrentAccount> currentAccounts = new ArrayList<CurrentAccount>();
 }
