b640119b7222e00b1b30ea0860fb048e8c9a29e8
Gary O Brien <gary.obrien@students.ittralee.ie>
2017-08-17 23:20:19 +0100

    Added getters and setters for Loan.java. Did not include setter for startDate as this will be set by the class constructor as the current date at instantiation

--- BankSystem/src/Loan.java
+++ BankSystem/src/Loan.java
@@ -9,4 +9,32 @@ public class Loan {
     private Date startDate;
     private boolean isCleared;
     private int loanAmount;
+
+    public int getLoanId() {
+        return loanId;
+    }
+
+    public void setLoanId(int loanId) {
+        this.loanId = loanId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public boolean isCleared() {
+        return isCleared;
+    }
+
+    public void setCleared(boolean cleared) {
+        isCleared = cleared;
+    }
+
+    public int getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(int loanAmount) {
+        this.loanAmount = loanAmount;
+    }
 }
