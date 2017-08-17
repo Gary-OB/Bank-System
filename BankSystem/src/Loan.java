0a6e556bc34afe1386f61b427660fe817f61b612
Gary O Brien <gary.obrien@students.ittralee.ie>
2017-08-17 23:18:36 +0100

    Added attributes to Loan.java

--- BankSystem/src/Loan.java
+++ BankSystem/src/Loan.java
@@ -1,5 +1,12 @@
+import java.util.Date;
+
 /**
  * Created by Gary on 17/08/2017.
  */
 public class Loan {
+
+    private int loanId;
+    private Date startDate;
+    private boolean isCleared;
+    private int loanAmount;
 }
